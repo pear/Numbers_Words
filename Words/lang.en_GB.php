ef5748f205878f8ea8488acd76a6d0dfcdf65f82
Daniel O'Connor <clockwerx@php.net>
2008-11-25 02:45:52 +0000

    Request #15145 - phpcs improvements
    git-svn-id: http://svn.php.net/repository/pear/packages/Numbers_Words/trunk@269648 c90b9560-bf6c-de11-be94-00142212c4b1

--- Words/lang.en_GB.php
+++ Words/lang.en_GB.php
@@ -47,7 +47,7 @@ class Numbers_Words_en_GB extends Numbers_Words
 {
 
     // {{{ properties
-    
+
     /**
      * Locale name
      * @var string
@@ -68,7 +68,7 @@ class Numbers_Words_en_GB extends Numbers_Words
      * @access public
      */
     var $lang_native = 'British English';
-    
+
     /**
      * The word for the minus sign
      * @var string
@@ -205,23 +205,23 @@ class Numbers_Words_en_GB extends Numbers_Words
      */
     function toWords($num, $power = 0, $powsuffix = '')
     {
-        $ret = '';        
-        
+        $ret = '';
+
         // add a minus sign
         if (substr($num, 0, 1) == '-') {
             $ret = $this->_sep . $this->_minus;
             $num = substr($num, 1);
         }
-        
+
         // strip excessive zero signs and spaces
         $num = trim($num);
         $num = preg_replace('/^0+/', '', $num);
-        
+
         if (strlen($num) > 3) {
             $maxp = strlen($num)-1;
             $curp = $maxp;
             for ($p = $maxp; $p > 0; --$p) { // power
-            
+
                 // check for highest power
                 if (isset($this->_exponent[$p])) {
                     // send substr from $curp to $p
@@ -246,9 +246,9 @@ class Numbers_Words_en_GB extends Numbers_Words
         } elseif ($num == 0 || $num == '') {
             return $this->_sep . $this->_digits[0];
         }
-    
+
         $h = $t = $d = 0;
-      
+
         switch(strlen($num)) {
         case 3:
             $h = (int)substr($num, -3, 1);
@@ -283,61 +283,61 @@ class Numbers_Words_en_GB extends Numbers_Words
         case 6:
             $ret .= $this->_sep . $this->_digits[$t] . 'ty';
             break;
-    
+
         case 8:
             $ret .= $this->_sep . 'eighty';
             break;
-    
+
         case 5:
             $ret .= $this->_sep . 'fifty';
             break;
-    
+
         case 4:
             $ret .= $this->_sep . 'forty';
             break;
-    
+
         case 3:
             $ret .= $this->_sep . 'thirty';
             break;
-    
+
         case 2:
             $ret .= $this->_sep . 'twenty';
             break;
-    
+
         case 1:
             switch ($d) {
             case 0:
                 $ret .= $this->_sep . 'ten';
                 break;
-    
+
             case 1:
                 $ret .= $this->_sep . 'eleven';
                 break;
-    
+
             case 2:
                 $ret .= $this->_sep . 'twelve';
                 break;
-    
+
             case 3:
                 $ret .= $this->_sep . 'thirteen';
                 break;
-    
+
             case 4:
             case 6:
             case 7:
             case 9:
                 $ret .= $this->_sep . $this->_digits[$d] . 'teen';
                 break;
-    
+
             case 5:
                 $ret .= $this->_sep . 'fifteen';
                 break;
-    
+
             case 8:
                 $ret .= $this->_sep . 'eighteen';
                 break;
             }
-            break; 
+            break;
         }
 
         if ($t != 1 && $d > 0) { // add digits only in <0>,<1,9> and <21,inf>
@@ -348,23 +348,23 @@ class Numbers_Words_en_GB extends Numbers_Words
                 $ret .= $this->_sep . $this->_digits[$d];
             }
         }
-  
+
         if ($power > 0) {
             if (isset($this->_exponent[$power])) {
                 $lev = $this->_exponent[$power];
             }
-    
+
             if (!isset($lev) || !is_array($lev)) {
                 return null;
             }
-     
+
             $ret .= $this->_sep . $lev[0];
         }
-    
+
         if ($powsuffix != '') {
             $ret .= $this->_sep . $powsuffix;
         }
-    
+
         return $ret;
     }
     // }}}
@@ -407,7 +407,7 @@ class Numbers_Words_en_GB extends Numbers_Words
         } else {
             $ret .= $this->_sep . $curr_names[0][0];
         }
-      
+
         if ($fraction !== false) {
             if ($convert_fraction) {
                 $ret .= $this->_sep . trim($this->toWords($fraction));
